006c1c6aca39de23a9b0365828ba6a4821575dc3
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2022-06-07 09:17:08 -0500

    arreglo stilos

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@
 document.getElementById('registrarse').onclick = function(){
 window.location.href="html/register.html";
 }
+
+
+
+document.getElementById('entrar').onclick = function(){
+
+let correo=document.getElementById('correo').value;
+let contra=document.getElementById('contrasena').value;
+
+if(correo==""){
+  alert("ingrese correo");
+}else if(contra==""){
+  alert("ingrese contraseña");
+}else{
+
+
+
+
+
+}
+
+
+}
