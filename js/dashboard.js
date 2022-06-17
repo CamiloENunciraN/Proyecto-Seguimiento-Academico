d2d270da326b8e42ed3d5c45360a573514bcf2d2
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2022-06-17 12:12:01 -0500

    pequeno cambio

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,3 +4,15 @@ function cargarRegistrarMateria() {
  let f=document.getElementById("ifram");
  f.src="formRegistrarMateria.html";
 }
+
+
+function volverInicio() {
+  location.href="dashboard.html";
+}
+
+
+
+function cerrarSesion() {
+  //cerrar la sesion y luego
+  location.href="../index.html";
+}
